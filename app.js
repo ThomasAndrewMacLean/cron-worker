a401749daa587a4340475a13563385190ed690d9
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2018-12-26 00:29:34 +0100

    testing?

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
-
+const fetch = require('node-fetch');
 const app = express();
 
 app.use(logger('dev'));
@@ -22,7 +22,69 @@ app.use((req, res, next) => {
 
 app.post('/updateSiteMap', (req, res) => {
     const challenge = req.body.challenge;
-    console.log(req.body);
+    const bot_id = 'BE5J35KLH';
+    const storyblokApi =
+        'api.storyblok.com/v1/cdn/stories?' + process.env.STORYBLOK_TOKEN;
+    const githubApi =
+        'https://api.github.com/repos/ThomasAndrewMacLean/project-blog/contents/sitemaproutes.js';
+
+    //check if its the storyblok bot that has published a story.
+    if (
+        req.body.event.bot_id === bot_id &&
+        req.body.event.text.contains('published the Story')
+    ) {
+        //get all the projects from the storyblok api
+        fetch(storyblokApi)
+            .then(res => res.json())
+            .then(result => {
+                const projects = result.stories.filter(story =>
+                    story.full_slug
+                        .contains('projects/')
+                        .map(story => story.full_slug)
+                );
+                const sitemaproutesString = `module.exports = ["${projects.join(
+                    '","'
+                )}"]`;
+
+                console.log(sitemaproutesString);
+                //send to the github api, and save as new siteroutes.
+                //This will trigger travis, and that will in turn deploy the new site.
+                fetch(githubApi, {
+                    method: 'PUT',
+                    body: JSON.stringify({
+                        message: 'my commit message',
+                        committer: {
+                            name: 'Scott Chacon',
+                            email: 'schacon@gmail.com'
+                        },
+                        content: 'bXkgbmV3IGZpbGUgY29udGVudHM='
+                    }),
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: 'Bearer ' + process.env.GITHUB_TOKEN
+                    }
+                })
+                    .then(res => res.json())
+                    .then(result => {
+                        console.log(result);
+                        //send a message to slack to say the job has been done.
+                        fetch(
+                            //get correct hook
+                            'https://hooks.slack.com/services/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',
+                            {
+                                method: 'POST',
+                                body: JSON.stringify({
+                                    text: `new pull request to be reviewed! PR:`
+                                }),
+                                headers: {
+                                    'Content-Type': 'application/json'
+                                }
+                            }
+                        );
+                    });
+            });
+    }
+
     res.status(200).json({
         challenge
     });
